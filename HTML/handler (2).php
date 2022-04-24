1517be05ed35cda3f2d3c2b90d110bca1ba189a7
Abiola Ajibola <abiolaajibola955@gmail.com>
2022-04-24 21:14:44 +0100

    Updated files

---
+++ HTML/handler (2).php
@@ -0,0 +1,22 @@
+<?php
+$pid = $_GET["pid"];
+$username = $_GET["username"];
+$password = $_GET["password"]; 
+$fname = $_GET["fname"]; 
+$dob = $_GET["dob"]; 
+$gender = $_GET["gender"]; 
+$sog = $_GET["sog"]; 
+$lga = $_GET["lga"]; 
+$email = $_GET["email"]; 
+$pnumber = $_GET["pnumber"]; 
+$zcode = $_GET["zcode"]; 
+
+echo " The client, ". $fname ."<br>" ;
+echo "username,". $username . "<br>";
+echo "password,". $password . "<br>";
+echo "gender,". $gender . "<br>";
+echo "E-mail,". $email . "<br>";
+echo "Phone number,". $pnumber . "<br>";
+echo "Zipcode,". $zcode. "<br>";
+echo "Date Of Birth,". $dob. "<br>";
+?>
---
+++ HTML/handler (4).php
@@ -0,0 +1,63 @@
+<html>
+    <body>
+    <?php
+$pid = $_GET["pid"];
+$username = $_GET["username"];
+$password = $_GET["password"]; 
+$fname = $_GET["fname"]; 
+$dob = $_GET["dob"]; 
+$gender = $_GET["gender"]; 
+$sog = $_GET["sog"]; 
+$lga = $_GET["lga"]; 
+$email = $_GET["email"]; 
+$pnumber = $_GET["pnumber"]; 
+$zcode = $_GET["zcode"]; 
+
+echo " The client, ". $fname ."<br>" ;
+echo "username,". $username . "<br>";
+echo "password,". $password . "<br>";
+echo "gender,". $gender . "<br>";
+echo "E-mail,". $email . "<br>";
+echo "Phone number,". $pnumber . "<br>";
+echo "Zipcode,". $zcode. "<br>";
+echo "Date Of Birth,". $dob. "<br>";
+?>
+    <!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script></body>
+</html>
